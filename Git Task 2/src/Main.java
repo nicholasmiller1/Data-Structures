3ad2da7a744cd41c7e3b0275b33e2c3a07d7e818
Nicholas Miller <MillerNC20@pwcs-edu.org>
2019-05-13 10:33:33 -0400

    Added eclipse project

---
+++ Git Task 2/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter two integers:");
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+		System.out.println("The average is: " + ((double) (a+c))/2);
+	}
+
+}
