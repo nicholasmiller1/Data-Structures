f73bd191c8d49b72863a8ee6b79755b83b82c647
Nicholas Miller <MillerNC20@pwcs-edu.org>
2019-05-17 12:31:24 -0400

    Updated mapfoot project

--- Mapfoot Project/MyWorld.java
+++ Mapfoot Project/MyWorld.java
@@ -2,26 +2,47 @@ import greenfoot.*;
 import java.util.*;
 
 public class MyWorld extends World {
-    GreenfootImage bg = this.getBackground();
-    List<Location> locations = new ArrayList<Location>();
+   private GreenfootImage bg;
+   private List<Location> locations = new ArrayList<Location>();
     
-    public MyWorld() {    
-        super(961, 604, 1);
-        setBackground("WorldMap.PNG");
-    }
+   public MyWorld() {    
+       super(961, 604, 1);
+       setBackground("WorldMap.PNG");
+   }
     
-    public void createRandomLocations( int num ) {
-        while ( num > 0 ) {
-            int x = (int) (Math.random() * 962);
-            int y = (int) (Math.random() * 605);
-            while ( bg.getColorAt(x,y).equals( new Color(0,0,0) ) ) {
-                x = (int) Math.random() * 962;
-                y = (int) Math.random() * 605;
-            }
-            Location l = new Location(x,y);
-            locations.add(l);
-            addObject(l,x,y);
-            num--;
-        }
-    }
+   public void createRandomLocations( int num ) {
+       bg = getBackground();
+       while ( num > 0 ) {
+           int x = (int) (Math.random() * (getWidth()-1));
+           int y = (int) (Math.random() * (getHeight()-1));
+           while ( bg.getColorAt(x,y).equals( Color.WHITE ) ) {
+              x = (int) (Math.random() * (getWidth()-1));
+              y = (int) (Math.random() * (getHeight()-1));
+           }
+           Location l = new Location(x,y);
+           locations.add(l);
+           addObject(l,x,y);
+           num--;
+       }
+   }
+   
+   public List<Edge> buildAllEdges() {
+       List<Edge> edges = new ArrayList<Edge>();
+       for ( int i = 0; i < locations.size(); i++ ) {
+           for ( int j = 0; j < locations.size(); j++ ) {
+              if ( !locations.get(i).equals( locations.get(j) ) ) {
+                 Edge e = new Edge( locations.get(i), locations.get(j) );
+                 edges.add(e);
+              }
+           }
+       }
+       drawEdges(edges);
+       return edges;
+   }
+   
+   public void drawEdges( List<Edge> edges ) {
+       for ( int i = 0; i < edges.size(); i++ ) {
+           edges.get(i).show(this);
+       }
+   }
 }
